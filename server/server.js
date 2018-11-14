643be84993eec5a2e24077cfab0c7f99b7ac49c2
Victor Souto <souto.victor@gmail.com>
2018-11-14 09:37:34 -0200

    initial project

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+
+app.use(express.static(publicPath));
+app.listen(3000, () => {
+
+    console.log('listening');
+});
+
+
+console.log(__dirname + '/../public');
+
+console.log();
