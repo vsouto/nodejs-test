dc909b0dbc84f9389a081b9fdbfbb40a0d19480e
Victor Souto <souto.victor@gmail.com>
2018-11-14 10:33:21 -0200

    ajustes ok

--- server/server.js
+++ server/server.js
@@ -1,16 +1,12 @@
 const path = require('path');
 const express = require('express');
-const publicPath = path.join(__dirname, '../public');
 
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
-app.listen(3000, () => {
 
-    console.log('listening');
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
-
-
-console.log(__dirname + '/../public');
-
-console.log();
